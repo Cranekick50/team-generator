5803efd7c1c9a7965712bad6db6c05a055c1e9cb
Cranekick50 <michael.srithapin@gmail.com>
2020-06-27 23:38:31 -0500

    updating questions

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,45 @@
 // TODO: Write code to define and export the Employee class
+export class Employee {
+    constructor(name, id, email, role) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = role;
+    }
+}
+
+
+function employeeDetails(answers){
+    let teamMember = new Employee
+}
+let questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee's ID?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email?",
+    },
+    {
+        type: "checkbox",
+        name: "role",
+        message: "What is the employee's role?",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager",
+        ]
+    },
+
+];
+
+inquirer.prompt(questions, employeeDetails);
+
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+import { Employee, Engineer } from './Employee.js';
+
+
+if (Employee.role === Engineer) { 
+    export class Engineer extends Employee;
+        constructor(name, id, email, role, gitHub)
+    let engQuestions = [
+        {
+            type: "input",
+            name: "gitHub",
+            message: "What is the GitHub ID?",
+        },
+    ];
+    inquirer.prompt(engquestions, answers);
+}
+
+
